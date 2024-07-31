2f11bc56626dadfc4c20af711ad84a1a9e845baa
areej009 <areej.fatima@aldaimsolutions.com>
2024-07-31 16:02:42 +0500

    all set

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  baseUrl: 'http://localhost:3000',
-  video: false,
+
   e2e: {
+   
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
