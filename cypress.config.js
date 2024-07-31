a0e99b2f2c817d5151b04349cbc5eea147fc008f
areej009 <areej.fatima@aldaimsolutions.com>
2024-07-31 16:25:56 +0500

    latest commit

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
 
   e2e: {
-   
+    "baseUrl": "http://localhost:3000",
+    "video": false,
+  
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
